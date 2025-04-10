51d1bf95d0b7090ddc64693f7c2ce17537f1a174
chandra2407 <chandra240797@gmail.com>
2025-04-10 17:06:18 +0530

    feat: adjust music toggle button position and size for better accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -97,10 +97,10 @@ function App() {
       </div>
 
       {/* Button to start music if autoplay is blocked */}
-      <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
         <button
           onClick={handleMusicToggle}
-          className="px-4 py-2 bg-gradient-to-t from-green-600 to-green-500 text-white rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-transform border border-green-700"
+          className="px-20 py-2 bg-gradient-to-t from-green-600 to-green-500 text-white rounded-full shadow-md hover:shadow-lg hover:scale-105 transition-transform border border-green-700"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
